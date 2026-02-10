ffcaabb5d45e0c5ec51570354775c7e27072ccff
Elias Chang <eliaschang.01@gmail.com>
2026-02-09 17:40:39 -0700

    space

--- src/autons.cpp
+++ src/autons.cpp
@@ -375,7 +375,6 @@ void measure_offsets() {
 // 2.5 seconds roughly for conveyor to take it to the top
 
 void match_auton() {
-  
   chassis.pid_odom_set(21_in, DRIVE_SPEED);
   setIntake(127);
   chassis.pid_wait();
