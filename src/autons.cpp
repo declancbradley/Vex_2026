a4971975f2f48dc2ffe07a9fe40c9cb531209cdd
declancbradley <declancbradley@gmail.com>
2026-02-09 17:26:19 -0700

    Odom Setup

--- src/autons.cpp
+++ src/autons.cpp
@@ -398,6 +398,6 @@ void skills_auton() {
   chassis.odom_xyt_set(0_in, 0_in, 0_deg);    // Set the current position, you can start at a specific position with this
   chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistency
 
-  chassis.pid_odom_set(24_in, 110); // speed is 0 to 127
+  chassis.pid_odom_set(24_in, 110);   // speed is 0 to 127
   chassis.pid_wait();
 }
